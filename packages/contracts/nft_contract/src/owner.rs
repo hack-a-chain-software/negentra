941320f58b09f0b3c67cf4c16593f5dacb236253
Eduardo <eduardogbg@gmail.com>
2022-06-01 10:36:35 -0300

    feat: add contract view function

--- packages/contracts/nft_contract/src/owner.rs
+++ packages/contracts/nft_contract/src/owner.rs
@@ -1,4 +1,26 @@
 use crate::*;
+use near_sdk::serde::Serialize;
+
+#[derive(Serialize)]
+struct ContractView {
+    pub item_count: u64,
+    pub perpetual_royalties: HashMap<AccountId, u128>,
+
+    pub mint_token: AccountId,
+    pub mint_cost: u128,
+}
+
+impl ContractView {
+    pub fn from(contract: &Contract) -> Self {
+        ContractView {
+            item_count: contract.item_count,
+            perpetual_royalties: contract.perpetual_royalties,
+
+            mint_token: contract.mint_token,
+            mint_cost: contract.mint_cost,
+        }
+    }
+}
 
 #[near_bindgen]
 impl Contract {
@@ -19,6 +41,7 @@ impl Contract {
         self.mint_token = mint_token;
         true
     }
+
     #[payable]
     pub fn change_perpetual_royalties(
         &mut self,
@@ -31,11 +54,9 @@ impl Contract {
         true
     }
 
-    /// ???
-    pub fn view(&self) -> &Contract {
+    pub fn view(&self) -> ContractView {
         self.only_owner();
-        assert_one_or_more_yocto();
 
-        return self;
+        ContractView::from(self)
     }
 }
