1323eedfc530bd2e940e4fa5338b30ed15304124
Eduardo <eduardogbg@gmail.com>
2022-06-02 11:20:50 -0300

    feat: enhance random draw & refund storage

--- packages/contracts/nft_contract/src/mint.rs
+++ packages/contracts/nft_contract/src/mint.rs
@@ -1,11 +1,13 @@
 use crate::*;
+use modified_contract_standards::non_fungible_token::utils::refund_deposit;
 use near_sdk::PromiseOrValue;
-use std::convert::TryFrom;
+use std::convert::{TryFrom, TryInto};
 use sum_tree::Operation;
 
 #[near_bindgen]
 impl Contract {
-    fn ft_on_transfer(
+    #[payable]
+    pub fn ft_on_transfer(
         &mut self,
         sender_id: ValidAccountId,
         amount: U128,
@@ -14,10 +16,18 @@ impl Contract {
         assert_eq!(env::predecessor_account_id(), "", "");
         assert!(amount.0 >= self.mint_cost, "");
 
-        let refund = amount.0 - self.mint_cost;
+        let initial_storage = env::storage_usage();
+        self.nft_mint();
+        let final_storage = env::storage_usage();
 
+        refund_deposit(initial_storage - final_storage);
+
+        let refund = amount.0 - self.mint_cost;
         PromiseOrValue::Value(U128(refund))
     }
+}
+
+impl Contract {
     /// Mint a new token with ID=`token_id` belonging to `receiver_id`.
     ///
     /// Since this example implements metadata, it also requires per-token metadata to be provided
@@ -26,14 +36,16 @@ impl Contract {
     ///
     /// `self.tokens.mint` will enforce `predecessor_account_id` to equal the `owner_id` given in
     /// initialization call to `new`.
-
     fn nft_mint(&mut self) -> Token {
         let account_id: AccountId = env::predecessor_account_id();
 
-        let random_seed: u64 = (*env::random_seed().get(0).unwrap() + 1).into();
+        let random_seed = *env::random_seed().get(0).unwrap();
         let total = self.item_amount_tree.root().unwrap_or(0);
 
-        let drawn_number = (u64::MAX / random_seed) % total; // hash random seed with total
+        let hash = env::keccak256(&[random_seed, (total % 256) as u8]);
+        let bytes = (hash[0..8]).try_into().unwrap();
+
+        let drawn_number = (u64::MAX / u64::from_be_bytes(bytes)) % total;
 
         let id = self.item_amount_tree.find(drawn_number).unwrap();
         let mut item = self.item_types.get(&id).unwrap();
