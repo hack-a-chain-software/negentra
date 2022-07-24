20656bfdf6d24e0e4acdfc4a063fd42335cdddda
Mateus Santana <42101423+1Mateus@users.noreply.github.com>
2022-07-24 00:30:19 -0300

    Update hero.tsx

--- packages/web/src/components/modules/landing/hero.tsx
+++ packages/web/src/components/modules/landing/hero.tsx
@@ -16,6 +16,7 @@ export function Hero() {
       <Container
         w="100vw"
         maxWidth="1920px"
+        minH="100vh"
         className="bg-[url('/images/hero-bg.png')] bg-[length:auto_auto] bg-[left_-20px_top_42px] bg-no-repeat"
       >
         <Container
